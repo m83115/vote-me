124bed6b151c99895d587b96b291d01a89a4eb3a
m83115 <m83115@yahoo.com.tw>
2021-12-18 23:45:19 +0800

    index delete confirm

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -39,6 +39,14 @@ class CandidatesController < ApplicationController
     end
   end
 
+  def destroy
+    @candidate = Candidate.find_by(id: params[:id])
+    @candidate.destroy
+
+    flash[:notice] = "Candidate deleted!"
+    redirect_to '/candidates'
+  end
+
   private
   def candidate_params
     params.require(:candidate).permit(:name, :party, :age, :politics)
