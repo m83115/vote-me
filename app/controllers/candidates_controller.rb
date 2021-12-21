f6c8e33b0768c061230c518e30c6ae1f59321834
m83115 <m83115@yahoo.com.tw>
2021-12-21 10:30:17 +0800

    Controller votes part 1

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -47,6 +47,16 @@ class CandidatesController < ApplicationController
     redirect_to '/candidates'
   end
 
+  def vote
+    @candidate = Candidate.find_by(id: params[:id])
+    # @candidate.votes = @candidate.votes + 1
+    @candidate.increment(:votes)
+    @candidate.save
+
+    flash[:notice] = "Votde!"
+    redirect_to '/candidates'
+  end
+
   private
   def candidate_params
     params.require(:candidate).permit(:name, :party, :age, :politics)
