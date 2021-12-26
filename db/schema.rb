fa54aabad0e210f10aba8af27697a0a7309bbf66
m83115 <m83115@yahoo.com.tw>
2021-12-26 23:14:14 +0800

    add_column part 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_22_154351) do
+ActiveRecord::Schema.define(version: 2021_12_26_145752) do
 
   create_table "candidates", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_12_22_154351) do
     t.integer "votes", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "vote_logs_count"
   end
 
   create_table "vote_logs", force: :cascade do |t|
